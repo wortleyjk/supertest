6c217d7ed7542e9967ac0067efab8ca2bd1dcbcc
wortleyjk-supertest <wortleyjk+supertest@gmail.com>
2024-04-06 23:21:25 -0700

    corrected the describe callback function

--- test/POneTests.js
+++ test/POneTests.js
@@ -2,7 +2,7 @@ import request from "supertest";
 import { expect } from "chai";
 const baseUrl = "www.reqres.com";
 
-describe("P1 Test Cases for API Automation",(done)=>{
+describe("P1 Test Cases for API Automation",()=>{
     it('Should give a 404 not found error for /nothing',(done)=>{
         request(baseUrl)
         .get("/nothing")
